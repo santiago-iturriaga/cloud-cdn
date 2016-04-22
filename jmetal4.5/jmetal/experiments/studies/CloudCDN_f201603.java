6069e6b546fb743ec3151307d7278ea5f5399a35
Santiago Iturriaga <santiago.iturriaga@gmail.com>
2016-04-22 17:45:53 +0200

    Added qsub low_0 for 12h, 24h, 48h

--- jmetal4.5/jmetal/experiments/studies/CloudCDN_f201603.java
+++ jmetal4.5/jmetal/experiments/studies/CloudCDN_f201603.java
@@ -39,10 +39,10 @@ public class CloudCDN_f201603 extends Experiment {
         try {
             Object[] problemParams;
 
-            int maxEval = 40000;
+            int maxEval = 60000;
             System.out.println("Num Evaluations: " + maxEval);
 
-            problemParams = new Object[]{"CloudCDNSolutionf201603Type",
+            problemParams = new Object[]{"CloudCDNSolutionf201603b25Type",
                 "../Instances/",
                 "../Instances/" + instance_type + "/data." + instance_number + "/",
                 "BestQoSSecure",
