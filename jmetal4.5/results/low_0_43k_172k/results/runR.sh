63bb0b1155e3cb811d1cf5831a0e38cae495092b
Santiago <santiago.iturriaga@gmail.com>
2018-11-28 23:50:22 -0300

    boxplots

---
+++ jmetal4.5/results/low_0_43k_172k/results/runR.sh
@@ -0,0 +1,15 @@
+cd CloudCDN_f201603_low_0_172800/R
+R < CloudCDN_f201603_low_0_172800.EPSILON.Boxplot.R --no-save
+R < CloudCDN_f201603_low_0_172800.SPREAD.Boxplot.R --no-save
+R < CloudCDN_f201603_low_0_172800.HV.Boxplot.R --no-save
+cd ../..
+cd CloudCDN_f201603_low_0_86400/R
+R < CloudCDN_f201603_low_0_86400.EPSILON.Boxplot.R --no-save
+R < CloudCDN_f201603_low_0_86400.SPREAD.Boxplot.R --no-save
+R < CloudCDN_f201603_low_0_86400.HV.Boxplot.R --no-save
+cd ../..
+cd CloudCDN_f201603_low_0_43200/R
+R < CloudCDN_f201603_low_0_43200.EPSILON.Boxplot.R --no-save
+R < CloudCDN_f201603_low_0_43200.SPREAD.Boxplot.R --no-save
+R < CloudCDN_f201603_low_0_43200.HV.Boxplot.R --no-save
+cd ../..
